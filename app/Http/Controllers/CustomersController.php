32be3a2232a482a9498aff55be59ecce45b03f9d
mirko93 <skopljak.mirko93@gmail.com>
2020-04-08 11:48:57 +0200

    customers list done

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -10,13 +10,10 @@ class CustomersController extends Controller
     public function list()
     {
 
-        $activeCustomers = Customer::where('active', 1)->get();
-        $inactiveCustomers = Customer::where('active', 0)->get();
+        $activeCustomers = Customer::active()->get();
+        $inactiveCustomers = Customer::inactive()->get();
 
-        return view('internals.customers', [
-            'activeCustomers' => $activeCustomers,
-            'inactiveCustomers' => $inactiveCustomers,
-        ]);
+        return view('internals.customers', compact('activeCustomers', 'inactiveCustomers'));
     }
 
     public function store()
@@ -28,12 +25,7 @@ class CustomersController extends Controller
             'active' => 'required',
         ]);
 
-        $customer = new Customer();
-        $customer->name = request('name');
-        $customer->email = request('email');
-        $customer->active = request('active');
-
-        $customer->save();
+        Customer::create($data);
 
         return back();
 
