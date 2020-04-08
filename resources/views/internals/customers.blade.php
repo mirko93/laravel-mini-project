fa4d175534559a76c778b391f38edcd0b27c5504
mirko93 <skopljak.mirko93@gmail.com>
2020-04-08 12:54:09 +0200

    added Company model and company database

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -31,6 +31,15 @@
                         <option value="0">Inactive</option>
                     </select>
                 </div>
+
+                <div class="form-group">
+                    <label for="company_id">Company</label>
+                    <select name="company_id" id="company_id" class="form-control">
+                        @foreach ($companies as $company)
+                            <option value="{{ $company->id }}">{{ $company->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
         
                 <button type="submit" class="btn btn-success">Add</button>
         
@@ -48,7 +57,7 @@
                 @foreach ($activeCustomers as $activeCustomer)
                     <li>
                         {{ $activeCustomer->name }}
-                        <span class="text-muted"> ({{ $activeCustomer->email }})</span>
+                        <span class="text-muted"> ({{ $activeCustomer->company->name }})</span>
                     </li>
                 @endforeach
             </ul>
@@ -60,10 +69,29 @@
                 @foreach ($inactiveCustomers as $inactiveCustomer)
                     <li>
                         {{ $inactiveCustomer->name }}
-                        <span class="text-muted"> ({{ $inactiveCustomer->email }})</span>
+                        <span class="text-muted"> ({{ $inactiveCustomer->company->name }})</span>
                     </li>
                 @endforeach
             </ul>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-12">
+            @foreach ($companies as $company)
+                <h3>{{ $company->name }}</h3>
+
+                <ul>
+                    @foreach ($company->customers as $customer)
+                    <li>{{ $customer->name }}</li>
+                @endforeach
+                </ul>
+
+                <hr>
+
+            @endforeach
         </div>
     </div>
 
