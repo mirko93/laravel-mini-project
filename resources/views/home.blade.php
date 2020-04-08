cdc81e3c9467987bf2ddca265fc448ff5442fc68
mirko93 <skopljak.mirko93@gmail.com>
2020-04-08 10:21:09 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <h1>Welcome to home page</h1>
+
+    
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\CustomersController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::view('about', 'about');
+Route::view('contact', 'contact');
+Route::get('customers', 'CustomersController@list');
