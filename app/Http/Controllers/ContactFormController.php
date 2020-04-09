e713dfe5682bff58106fb2d38990da859ff44e44
mirko93 <skopljak.mirko93@gmail.com>
2020-04-09 10:53:10 +0200

    contact form function add (Mail sender)

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -24,6 +24,6 @@ class ContactFormController extends Controller
         // Send An Email
         Mail::to('test@test.com')->send(new ContactFormMail($data));
 
-        return redirect('contact');
+        return redirect('contact')->with('message', 'Thanks for your message. We\'ll be in touch.');
     }
 }
